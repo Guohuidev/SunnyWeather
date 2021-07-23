75ce8cc90ebc43e187168ba4d519e6dbfd53920d
Liuhaiqing <957443837@qq.com>
2021-07-23 23:16:45 +0800

    add .

--- app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
@@ -1,6 +1,5 @@
 package com.sunnyweather.android.ui.place
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -15,7 +14,6 @@ import androidx.lifecycle.ViewModelProvider
 
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.sunnyweather.android.MainActivity
 import com.sunnyweather.android.R
 
 
